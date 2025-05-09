babeaf69b2b5853d3065976bbd5bab0b4fb6e7b4
habibiezkrillh <zikrillahhabibie@gmail.com>
2025-05-09 16:59:18 +0800

    initialized laravel project

---
+++ backend-imu/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+
+Route::middleware([
+    EnsureFrontendRequestsAreStateful::class,
+    'auth:sanctum',
+])->group(function () {
+    Route::get('/me', function () {
+        return auth()->user();
+    });
+
+    // Route lainnya yang butuh otentikasi
+});
