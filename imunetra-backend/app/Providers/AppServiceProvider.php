0425f9bedf28b8e4054f248b572b6bd4404f7e69
leowdij08 <158173182+leowdij08@users.noreply.github.com>
2025-06-03 10:25:12 +0800

    AppServiceProvider

--- imunetra-backend/app/Providers/AppServiceProvider.php
+++ imunetra-backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Repositories\ChatRepositoryInterface;
+use App\Models\Repositories\ChatRepository;
+use App\Models\Repositories\DataPasienRepositoryInterface;
+use App\Models\Repositories\DataPasienRepository;
+use App\Models\Repositories\EventRepositoryInterface;
+use App\Models\Repositories\EventRepository;
+use App\Models\Repositories\HasilPasienRepositoryInterface;
+use App\Models\Repositories\HasilPasienRepository;
+use App\Models\Repositories\PesanChatRepositoryInterface;
+use App\Models\Repositories\PesanChatRepository;
+use App\Models\Repositories\RiwayatRelawanRepositoryInterface;
+use App\Models\Repositories\RiwayatRelawanRepository;
+use App\Models\Repositories\RiwayatTenagaMedisRepositoryInterface;
+use App\Models\Repositories\RiwayatTenagaMedisRepository;
+use App\Models\Repositories\UserRelawanRepositoryInterface;
+use App\Models\Repositories\UserRelawanRepository;
+use App\Models\Repositories\UserTenagaMedisRepositoryInterface;
+use App\Models\Repositories\UserTenagaMedisRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +29,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ChatRepositoryInterface::class, ChatRepository::class);
+        $this->app->bind(DataPasienRepositoryInterface::class, DataPasienRepository::class);
+        $this->app->bind(EventRepositoryInterface::class, EventRepository::class);
+        $this->app->bind(HasilPasienRepositoryInterface::class, HasilPasienRepository::class);
+        $this->app->bind(PesanChatRepositoryInterface::class, PesanChatRepository::class);
+        $this->app->bind(RiwayatRelawanRepositoryInterface::class, RiwayatRelawanRepository::class);
+        $this->app->bind(RiwayatTenagaMedisRepositoryInterface::class, RiwayatTenagaMedisRepository::class);
+        $this->app->bind(UserRelawanRepositoryInterface::class, UserRelawanRepository::class);
+        $this->app->bind(UserTenagaMedisRepositoryInterface::class, UserTenagaMedisRepository::class);
+
     }
 
     /**
