e9cef5fca8dbe8884c9298177d2aa25482971a4f
leowdij08 <158173182+leowdij08@users.noreply.github.com>
2025-06-10 08:56:16 +0800

    Pembuatan Controller

--- imunetra-backend/routes/api.php
+++ imunetra-backend/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Laravel\Sanctum\HasApiTokens;
-use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
